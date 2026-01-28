5216ff245110339b2970fe57e362b41ed5980f39
chheniqu <charlotte.1.hq@gmail.com>
2026-01-28 13:34:15 +0100

    avancement

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,12 +1,37 @@
 // groups auth-related controllers and services together
 // defines which controllers and services exist and specifies what this module exports to other parts of the application
 
+// importation du decorateur @Module qui nous sert a definir un modele
+// un decorateur indique a NestJS comment utiliser ma classe
+// cette classe est un module qui regroupe controller et service
 import { Module } from '@nestjs/common';
+// importation du service, le module doit savoir quel service il gere
 import { AuthService } from './auth.service';
+// importation du controller, le module doit savoir quel controller il gere
 import { AuthController } from './auth.controller';
+// importation du module JWT pour creer et verifier les tokens 
+import { JwtModule } from '@nestjs/jwt';
+// Pasport = systeme d'authentificaion standard
+import { PassportModule } from '@nestjs/passport';
+// importation du service prisma pour acceder a la base de donnee
+// on va l'utiliser pour creer un utilisateur, chercher un utilisateur et verifier un login
+import { PrismaService } from '../prisma.service';
 
+// declaration d'un module NestJS, module = bloc logique
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  // liste des autres nodules utilises par AuthModule, ajout de foncitonnalites externes
+  imports: [
+    PassportModule,
+    // on appelle la fonction register du module JWT avec les parametres suivants 
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      // le token expire apres 1 heure
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService, PrismaService],
+  controllers: [AuthController],
 })
+// on exporte le module pour que AppModule puisse l'utiliser
+
 export class AuthModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,22 @@
 // handles password hashing (bcrypt), user verification, JWT creation, and DB access via Prisma
 
-import { Injectable } from '@nestjs/common';
+// BadRequestException = erreur 400, UnauthorizedException = erreur 401
+import { Injectable, BadRequestException, UnauthorizedException } from '@nestjs/common';
+// sert a parler a db
+import { PrismaService } from '../prisma.service';
+// sert pour les erreurs Prisma (pour reconnaitre erreur P002)
+import { PrismaClient } from "@prisma/client";
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+// donnes recues du front
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	// demande a nest d'injecter ces services 
+	constructor(
+		private prisma: PrismaService,
+		private JwtService: JwtService,
+	) {}
+}
