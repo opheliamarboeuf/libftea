b4a25074d06becdd2b53b4944530f9311172f2aa
chheniqu <charlotte.1.hq@gmail.com>
2026-02-26 19:05:34 +0100

    creation battle

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,6 +11,7 @@ import { FriendsModule } from './friends/friends.module';
 import { UsersModule } from './user/users.module';
 import { ProfileModule } from './profile/profile.module';
 import { PostsModule } from './posts/posts.module';
+import { TournamentModule } from './tournament/tournament.module';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { PostsModule } from './posts/posts.module';
 	UsersModule,
 	ProfileModule,
 	PostsModule,
+	TournamentModule,
   ],
   controllers: [AppController], 
   providers: [
---
+++ backend/src/tournament/tournament.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { TournamentService } from './tournament.service';
+import { CreateTournamentDto } from './dto/create-tournament.dto';
+
+@Controller('tournament')
+export class TournamentController {
+	// injection de dependances, indique a nestJS que l'on va utiliser TournamentService
+	// on le stocke dans une variable privee tournamentService de type TournamentService
+	// readonly pour ne pas la modifier, provate pour que la variable soit accessible seulement dans cette classe
+	constructor(private readonly tournamentService: TournamentService) {}
+	@Post()
+	// @Body() on prend le corps de la requete HTTP
+	// data = nom de la variable, any = type TS 
+	createTournament(@Body() data: CreateTournamentDto) {
+		return this.tournamentService.createTournament(data);
+	}
+	@Get('current')
+	getCurrentTournament() {
+		return this. tournamentService.getCurrentTournament();
+	}
+}
