9b5c194bec78f1be981aeb3fc5cebcc50c9cf9d7
HENIQUE Charlotte <charlotte@MacBook-Air-de-HENIQUE.local>
2026-03-23 12:48:45 +0100

    main http

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	app.enableCors({
-		origin: process.env.FRONTEND_URL,
+		origin: 'http://localhost:5173',
 		credentials: true,
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
